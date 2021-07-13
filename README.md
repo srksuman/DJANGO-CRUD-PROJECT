d8a4532eb4878c13001c09c1c7f1f830b581c823
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2021-07-13 08:10:28 +0545

    INITIAL COMMIT

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple Django CRUD Project
---
+++ operations/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OperationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'operations'

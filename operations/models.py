5fced69229a0f74f93e776939a252106bff57b9a
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2021-07-13 10:36:54 +0545

    model is created and migrations are done!

--- operations/models.py
+++ operations/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    student_field = (
+        ('BIT','BIT'),
+        ('BBA','BBA'),
+        ('MSCIT','MSCIT'),
+        ('CSIT','CSIT')
+    )
+    full_name = models.CharField(max_length=70)
+    address = models.CharField(max_length=70)
+    phone_number = models.IntegerField()
+    field = models.CharField(max_length=70,choices=student_field)
+    picture = models.ImageField(upload_to='images/')

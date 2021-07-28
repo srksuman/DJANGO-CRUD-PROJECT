1d9d802215b0a37ef0ffe3cf36a12960327e6426
Suman Raj Khanal <67781881+srksuman@users.noreply.github.com>
2021-07-28 14:24:16 +0545

    Update settings.py

--- crud/settings.py
+++ crud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!4m5$*s5j%p!vz!%ci2zf@52el$-!el(#4p6=$&*2o5z-z($^5'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
